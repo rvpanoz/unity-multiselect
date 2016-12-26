20e9f8618f22a3fe7ccdfc5170e6e162b2f06d83
root <rvpanoz@gmail.com>
2016-12-26 12:38:00 +0200

    added app-local-js

---
+++ app-local.js
@@ -0,0 +1,12 @@
+define([
+  'lib','app',
+  'multi-select',
+  'libc/jquery.mask/jquery.mask'],function() {
+
+    lib.dateType = 'string';
+    lib.number.ds='.';
+    lib.number.dp=',';
+    lib.autoMask = true;
+    app.homeUrl = {cls:'home'};
+
+  });
